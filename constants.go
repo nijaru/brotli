857975c8afece0663a397f9938f63d0122d3144f
Nick Russo <nijaru7@gmail.com>
2026-04-02 02:36:52 -0700

    refactor(matchfinder): trim unused constants and note SA4006

--- constants.go
+++ constants.go
@@ -6,22 +6,12 @@ package brotli
    See file LICENSE for detail or copy at https://opensource.org/licenses/MIT
 */
 
-/* Specification: 7.3. Encoding of the context map */
-const contextMapMaxRle = 16
-
-/* Specification: 2. Compressed representation overview */
-const maxNumberOfBlockTypes = 256
-
 /* Specification: 3.3. Alphabet sizes: insert-and-copy length */
 const numLiteralSymbols = 256
 
 const numCommandSymbols = 704
 
 const numBlockLenSymbols = 26
-
-const maxContextMapSymbols = (maxNumberOfBlockTypes + contextMapMaxRle)
-
-const maxBlockTypeSymbols = (maxNumberOfBlockTypes + 2)
 
 /* Specification: 3.5. Complex prefix codes */
 const repeatPreviousCodeLength = 16
@@ -45,18 +35,11 @@ const numDistanceShortCodes = 16
 
 const maxNpostfix = 3
 
-const maxNdirect = 120
-
 const maxDistanceBits = 24
 
-func distanceAlphabetSize(NPOSTFIX uint, NDIRECT uint, MAXNBITS uint) uint {
-	return numDistanceShortCodes + NDIRECT + uint(MAXNBITS<<(NPOSTFIX+1))
+func distanceAlphabetSize(nPostfix, nDirect, maxNBits uint) uint {
+	return numDistanceShortCodes + nDirect + uint(maxNBits<<(nPostfix+1))
 }
-
-/* numDistanceSymbols == 1128 */
-const numDistanceSymbols = 1128
-
-const maxDistance = 0x3FFFFFC
 
 const maxAllowedDistance = 0x7FFFFFFC
 
@@ -71,7 +54,3 @@ const distanceContextBits = 2
 /* Number of slack bytes for window size. Don't confuse
    with BROTLI_NUM_DISTANCE_SHORT_CODES. */
 const windowGap = 16
-
-func maxBackwardLimit(W uint) uint {
-	return (uint(1) << W) - windowGap
-}
