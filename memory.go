59f9327ba2f84e2a60c2778a99739e798fe86cb8
Nick Russo <nijaru7@gmail.com>
2026-04-25 14:11:44 -0700

    chore(root): remove dead files from root package
    Remove unused files:
    - buffer_pool.go: buffer pooling not used after encoder move
    - find_match_length.go: duplicate of common.FindMatchLengthWithLimit
    - memory.go, params.go, platform.go, stubs.go, util.go: legacy helpers
    
    Root package now has 5 files:
    - writer.go (206 lines): Writer facade
    - reader.go (20 lines): thin decoder wrapper
    - http.go (189 lines): HTTP compressor helpers
    - brotli_test.go (1065 lines): tests
    - example_test.go (53 lines): examples
    
    All tests pass.

--- memory.go
+++
@@ -1,7 +0,0 @@
-package brotli
-
-/* Copyright 2016 Google Inc. All Rights Reserved.
-
-   Distributed under MIT license.
-   See file LICENSE for detail or copy at https://opensource.org/licenses/MIT
-*/
